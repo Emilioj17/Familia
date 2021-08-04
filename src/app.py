f6f5d6cb7e878dfc4358045dab93764f0334c8ec
Emilioj17 <ejjelves@gmail.com>
2021-08-04 18:11:51 -0400

    Ok, funcionando y pasa test

--- src/app.py
+++ src/app.py
@@ -30,13 +30,55 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+    response_body = {}
+    for index in range(len(members)):
+        response_body["member" + str(index)] = members[index]
+        
+    return jsonify(list(response_body)), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_one_member(id):
+    member = jackson_family.get_member(id)
+
+    if member is not None:
+        response_body = {
+            "id": member['id'],
+            "first_name": member['first_name'],
+            "age": member['age'],
+            "lucky_numbers": member['lucky_numbers']
+        }
+        return jsonify(response_body), 200
+    else:
+        return jsonify(Error="Not founded")
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    if request.json.get('first_name') is None or request.json.get('age') is None or request.json.get('lucky_numbers') is None:
+        return jsonify(), 400
+    first_name = request.json.get('first_name')
+    age = int(request.json.get('age'))
+    lucky_numbers = request.json.get('lucky_numbers')
+    id = request.json.get('id', None)
+
+    member = {
+        'first_name': first_name,
+        'age': age,
+        'lucky_numbers': lucky_numbers,
+        'id': id
     }
 
+    jackson_family.add_member(member)
 
-    return jsonify(response_body), 200
+    return jsonify(), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+
+    resp = jackson_family.delete_member(id)
+    if resp == True:
+        return jsonify({'done': True}), 200
+    else:
+        return jsonify(Error='Not founded')
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
